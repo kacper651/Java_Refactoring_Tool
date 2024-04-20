b6c81f0eb7df8eaa98a9fef87742d54e317a8327
kacper651 <kacper.wroblewski651@gmail.com>
2024-04-20 19:25:02 +0200

    Refactoring

--- src/main/java/rewriter/RefactoringTool.java
+++ src/main/java/rewriter/RefactoringTool.java
@@ -13,25 +13,29 @@ public class RefactoringTool {
 
     public static void main(String[] args) {
 
+        // read input file
         CharStream input = null;
         try {
-            input = CharStreams.fromFileName("src/rewriter/InsertDocListener.java");
+            input = CharStreams.fromFileName("src/main/java/rewriter/InsertDocListener.java");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        JavaLexer lex = new JavaLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lex);
-        JavaParser par = new JavaParser(tokens);
 
-        ParseTree tree = par.compilationUnit();
+        // create lexer, parser, and parse tree
+        JavaLexer lexer = new JavaLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        JavaParser parser = new JavaParser(tokens);
+
+        // create walker and listener
+        ParseTree tree = parser.compilationUnit();
         ParseTreeWalker walker = new ParseTreeWalker();
         InsertDocListener inserter = new InsertDocListener(tokens);
         walker.walk(inserter,tree);
         System.out.println(inserter.rewriter.getText());
         try {
-            var wr = new FileWriter("Out.java");
-            wr.write(inserter.rewriter.getText());
-            wr.close();
+            var writer = new FileWriter("Out.java");
+            writer.write(inserter.rewriter.getText());
+            writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/rewriter/RenameVariableListener.java
+++ src/main/java/rewriter/RenameVariableListener.java
@@ -1,37 +1,29 @@
 package rewriter;
 
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStreamRewriter;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class RenameVariableListener extends JavaParserBaseListener {
     private Map<String, String> variableMap = new HashMap<>();
-
-    // Override methods to capture variable declarations and references
-    // Implement logic to rename variables as needed
-
-    public void renameVariable(String oldName, String newName) {
-        variableMap.put(oldName, newName);
-    }
+    private final CommonTokenStream tokenStream;
+    TokenStreamRewriter rewriter;
 
     // w jakiej funkcji jaka zmienna / xpath do szukania nazwy funkcji / trzeba przechowywac id funkcji
-    // mozna tez to wywolac w drzewie wynikowym / parsetreepattern do wyszukiwania regul (w dokumentacji jest opisane to obok siebie)
-    // 
+    // mozna tez to wywolac w drzewie wynikowym / parsetreepattern do wyszukiwania regul
+    // (w dokumentacji jest opisane to obok siebie)
 
 
     @Override
     public void exitVariableDeclaratorId(JavaParser.VariableDeclaratorIdContext ctx) {
-//        String oldName = ctx.identifier().getText();
-//        if (variableMap.containsKey(oldName)) {
-//            ctx.identifier().setText(variableMap.get(oldName));
-//        }
+
     }
 
     @Override
     public void exitIdentifier(JavaParser.IdentifierContext ctx) {
-//        String oldName = ctx.getText();
-//        if (variableMap.containsKey(oldName)) {
-//            ctx.IDENTIFIER().setText(variableMap.get(oldName));
-//        }
+
     }
 }
 
