eded7f3c794f4996ca10b3d1f72b84ecdf245759
kacper651 <kacper.wroblewski651@gmail.com>
2024-04-27 11:41:22 +0200

    Finally working parameter changing system

--- src/main/java/rewriter/ChangeMethodParametersListener.java
+++ src/main/java/rewriter/ChangeMethodParametersListener.java
@@ -7,9 +7,7 @@ import org.antlr.v4.runtime.TokenStreamRewriter;
 import java.util.HashMap;
 
 enum OpType {
-    ADD,
-    REMOVE,
-    CHANGE
+    ADD, REMOVE, CHANGE
 }
 
 public class ChangeMethodParametersListener extends JavaParserBaseListener {
@@ -19,10 +17,7 @@ public class ChangeMethodParametersListener extends JavaParserBaseListener {
     String methodName;
     OpType opType;
 
-    public ChangeMethodParametersListener(HashMap<String, String> parameterMap,
-                                          CommonTokenStream tokens,
-                                          String methodName,
-                                          OpType opType) {
+    public ChangeMethodParametersListener(HashMap<String, String> parameterMap, CommonTokenStream tokens, String methodName, OpType opType) {
         this.parameterMap = parameterMap;
         this.rewriter = new TokenStreamRewriter(tokens);
         this.methodName = methodName;
@@ -34,20 +29,41 @@ public class ChangeMethodParametersListener extends JavaParserBaseListener {
         if (interesting != null && interesting.equals(methodName)) {
             if (opType == OpType.ADD) {
                 for (String key : parameterMap.keySet()) {
-                    rewriter.insertAfter(ctx.LPAREN().getSymbol(), parameterMap.get(key) + ", ");
-                }
-            } else if (opType == OpType.REMOVE) {
-                rewriter.replace(ctx.formalParameterList().start, ctx.formalParameterList().stop, "");
-            } else if (opType == OpType.CHANGE) {
-                for (String key : parameterMap.keySet()) {
-                    rewriter.replace(ctx.start, ctx.stop, parameterMap.get(key));
+                    rewriter.insertAfter(
+                            ctx.formalParameterList().formalParameter(ctx.formalParameterList().formalParameter().size() - 1).stop,
+                            ", " + parameterMap.get(key));
+            }
+        } else if (opType == OpType.REMOVE) {
+            rewriter.replace(
+                    ctx.formalParameterList().start,
+                    ctx.formalParameterList().stop,
+                    "");
+        } else if (opType == OpType.CHANGE) {
+            for (String key : parameterMap.keySet()) {
+                for (int i = 0; i < ctx.formalParameterList().formalParameter().size(); i++) {
+                    String paramName = ctx.formalParameterList()
+                            .formalParameter(i)
+                            .typeType()
+                            .getText()
+                            + " " +
+                            ctx.formalParameterList()
+                                    .formalParameter(i)
+                                    .variableDeclaratorId()
+                                    .getText();
+                    if (paramName.equals(key)) {
+                        rewriter.replace(
+                                ctx.formalParameterList().formalParameter(i).start,
+                                ctx.formalParameterList().formalParameter(i).stop,
+                                parameterMap.get(key));
+                    }
                 }
             }
         }
     }
+}
 
-    @Override
-    public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
-        interesting = ctx.identifier().IDENTIFIER().getText();
-    }
+@Override
+public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
+    interesting = ctx.identifier().IDENTIFIER().getText();
+}
 }
