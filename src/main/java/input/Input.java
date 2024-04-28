59790eb5e6744b2f8e995ba2fbc2fa89d192eae9
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2024-04-29 00:14:15 +0200

    Skonczone, smiga, sprawdzcie sami

--- src/main/java/input/Input.java
+++ src/main/java/input/Input.java
@@ -1,10 +1,57 @@
 package input;
 
 class a{
+
+    class b {
+        private int c;
+    }
     private int d;
     private String e;
+    public a(){
+        this.d = 5;
+        this.e = "XD";
+    }
     public a(int d){
         this.d = d;
+        e = "???";
     }
-    public int method(){ return d+1; }
+    public a(String e){
+        this.e = e;
+        d = 0;
+    }
+    public a(int d, String e){
+        this.d = d;
+        this.e = e;
+    }
+
+    public int method1(int b){
+        int c = 5;
+        d+=1;
+
+        for (int x = 1; x < d; x++){
+            c+=x;
+            c+=d;
+        }
+
+        while  (d < 10){
+            e = "xd";
+            do {
+              String z = e + "xd";
+            } while (d != 3);
+        }
+
+        switch (d){
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+        return d+1;
+    }
+
+    public int method2(){
+        return method1(d);
+    }
 }
--- src/main/java/rewriter/RenameClassFieldListener.java
+++ src/main/java/rewriter/RenameClassFieldListener.java
@@ -3,14 +3,27 @@ package rewriter;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.TokenStreamRewriter;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Stack;
 
+//  this listener works on the basis of an assumption, that no local variable
+//  will have the same name as a class field
 public class RenameClassFieldListener extends JavaParserBaseListener{
 
-    public Map<String, String> classFieldMap;
-    TokenStreamRewriter rewriter;
-    String classScope;
-    String interesting = null;
+    private final Map<String, String> classFieldMap;
+    public final TokenStreamRewriter rewriter;
+    private final String classScope;
+
+    // guards against renaming some local variable with enterExpression - works on the basis of the fact,
+    // that the class fields and constructors come first in a class, before random methods
+    private final HashSet<String> foundClassFieldsSet = new HashSet<>();
+
+    // stack to keep track of the current class - I push the name of the class as I start traversing it,
+    // and I pop it at the end - it makes sure, that I don't accidentaly rename something in an innner/outer class
+    // against my will if something will match with the classFieldMap
+    private final Stack<String> currentClassStack = new Stack<>();
+
 
     public RenameClassFieldListener(Map<String, String> classFieldMap, CommonTokenStream tokens, String classScope){
         this.classFieldMap = classFieldMap;
@@ -20,17 +33,20 @@ public class RenameClassFieldListener extends JavaParserBaseListener{
 
     @Override
     public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
-        interesting = ctx.identifier().IDENTIFIER().getText();
+        String className = ctx.identifier().IDENTIFIER().getText();
+        currentClassStack.push(className);
     }
 
     @Override
     public void enterFieldDeclaration(JavaParser.FieldDeclarationContext ctx) {
-        if (interesting.equals(classScope) &&
-            ctx.variableDeclarators().variableDeclarator() != null){
+        if (!currentClassStack.isEmpty() &&
+                currentClassStack.peek().equals(classScope) &&
+                ctx.variableDeclarators().variableDeclarator() != null){
             for (var variableDeclarator : ctx.variableDeclarators().variableDeclarator()){
                 String name = variableDeclarator.variableDeclaratorId().identifier().IDENTIFIER().getText();
                 if (name != null){
                     if (classFieldMap.containsKey(name)){
+                        foundClassFieldsSet.add(name);
                         rewriter.replace(variableDeclarator.variableDeclaratorId().identifier().IDENTIFIER().getSymbol(),
                                 classFieldMap.get(name));
                     }
@@ -41,8 +57,9 @@ public class RenameClassFieldListener extends JavaParserBaseListener{
 
     @Override
     public void enterConstructorDeclaration(JavaParser.ConstructorDeclarationContext ctx){
-        if (interesting.equals(classScope) &&
-            ctx.block().blockStatement() != null){
+        if (!currentClassStack.isEmpty() &&
+                currentClassStack.peek().equals(classScope) &&
+                ctx.block().blockStatement() != null){
             for (var blockStatementElem : ctx.block().blockStatement()){
                 if (blockStatementElem.statement().expression() != null){
 
@@ -52,8 +69,10 @@ public class RenameClassFieldListener extends JavaParserBaseListener{
                             for (var innerExpression : expressionElem.expression()){
                                 if (innerExpression.expression() != null
                                     && innerExpression.identifier() != null){
+                                        String name = innerExpression.identifier().IDENTIFIER().getText();
+                                        foundClassFieldsSet.add(name);
                                         rewriter.replace(innerExpression.identifier().IDENTIFIER().getSymbol(),
-                                                classFieldMap.get(innerExpression.identifier().IDENTIFIER().getText()));
+                                                classFieldMap.get(name));
                                 }
 
                             }
@@ -67,13 +86,36 @@ public class RenameClassFieldListener extends JavaParserBaseListener{
         }
     }
 
-
     @Override
-    public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx){
-        // TODO: implement changing class fields in methods
+    public void enterFormalParameter(JavaParser.FormalParameterContext ctx) {
+        if (ctx.variableDeclaratorId() != null && ctx.variableDeclaratorId().identifier() != null){
+            String name = ctx.variableDeclaratorId().identifier().IDENTIFIER().getText();
+            if (classFieldMap.containsKey(name) &&
+                    !currentClassStack.isEmpty() &&
+                    currentClassStack.peek().equals(classScope) &&
+                    foundClassFieldsSet.contains(name)){
+                rewriter.replace(ctx.variableDeclaratorId().identifier().IDENTIFIER().getSymbol(),
+                        classFieldMap.get(name));
+            }
+        }
     }
 
-    // TODO: guard from inner classes bullshit
+    @Override
+    public void enterExpression(JavaParser.ExpressionContext ctx) {
+        if (ctx.start == null) return;
+        var variableName = ctx.start.getText();
+        if (classFieldMap.containsKey(variableName) &&
+                !currentClassStack.isEmpty() &&
+                currentClassStack.peek().equals(classScope) &&
+                foundClassFieldsSet.contains(variableName)) {
+            rewriter.replace(ctx.start, classFieldMap.get(variableName));
+        }
+    }
+
+    @Override
+    public void exitClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
+        currentClassStack.pop();
+    }
 
 }
 
